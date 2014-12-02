9a51bbd9575ab0cd4e903259c1d2f2b6825f0fdd
jmcquintas@gmail.com <jmcquintas@gmail.com@fe6ad8b2-7dd7-65bc-c6bf-69d4feb7af03>
2014-12-02 03:22:53 +0000

    update final do -w3 -es2

--- source/c.bat
+++ source/c.bat
@@ -1,15 +1,13 @@
 @echo off
-echo enquanto não terminar o ajuste -w3 -es2
-echo a opção é compilar digitando c -w0 -es0
 echo.
 echo Estes arquivos de compilacao foram criados apenas pra teste
 echo.
 echo fazem parte do Harbour hbcurl.ch, hbcompat.ch, hbxml.ch
-echo apague desta pasta e use os arquivos do seu próprio Harbour
+echo apague desta pasta e use os arquivos do seu proprio Harbour
 echo.
-echo o arquivo hbnfe.ch é o mesmo da pasta ..\include copiado só pra facilitar compilação
+echo o arquivo hbnfe.ch e o mesmo da pasta ..\include copiado so pra facilitar compilacao
 echo.
-echo no hbp está usando workdir=d:\temp, ajuste para sua configuração
+echo no hbp esta usando workdir=d:\temp, ajuste para sua configuracao
 echo.
 pause
 hbmk2 hbnfe -xhb %1 %2 %3 %4
